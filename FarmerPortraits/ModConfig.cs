c8827281f5ce15b7099511f66684977b3abcf02e
Mistyspring <105533011+misty-spring@users.noreply.github.com>
2024-03-21 18:42:31 -0300

    Update FarmerPortraits unofficial

--- FarmerPortraits/ModConfig.cs
+++ FarmerPortraits/ModConfig.cs
@@ -1,17 +1,14 @@
-﻿
-using StardewModdingAPI;
-
-namespace FarmerPortraits
+﻿namespace FarmerPortraits
 {
     public class ModConfig
     {
         public bool EnableMod { get; set; } = true;
         public bool ShowWithQuestions { get; set; } = true;
-        public bool ShowWithEvents { get; set; } = false;
-        public bool ShowWithNPCPortrait { get; set; } = true;
-        public bool ShowMisc { get; set; } = false;
-        public bool FacingFront { get; set; } = false;
-        public bool UseCustomPortrait { get; set; } = false;
+        public bool ShowWithEvents { get; set; }
+        public bool ShowWithNpcPortrait { get; set; } = true;
+        public bool ShowMisc { get; set; }
+        public bool FacingFront { get; set; }
+        public bool UseCustomPortrait { get; set; } = true;
         public bool UseCustomBackground { get; set; } = true;
     }
 }
